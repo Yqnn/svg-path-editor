a2555a23cbb1a2edd37ab1fdc291a72a43d05e9b
Yann Armelin <yann@armelin.fr>
2020-10-31 21:01:19 +0100

    Added support of scentific notation in input path

--- src/app/svg-parser.ts
+++ src/app/svg-parser.ts
@@ -1,7 +1,7 @@
 const kCommandTypeRegex = /^[\t\n\f\r ]*([MLHVZCSQTAmlhvzcsqta])[\t\n\f\r ]*/;
 const kFlagRegex = /^[01]/;
-const kNumberRegex = /^(([0-9]+(\.[0-9]*)?)|\.[0-9]+)/;
-const kCoordinateRegex = /^[+-]?(([0-9]+(\.[0-9]*)?)|\.[0-9]+)/;
+const kNumberRegex = /^[+-]?(([0-9]*\.[0-9]+)|([0-9]+\.)|([0-9]+))([eE][+-]?[0-9]+)?/;
+const kCoordinateRegex = kNumberRegex;
 const kCommaWsp = /^(([\t\n\f\r ]+,?[\t\n\f\r ]*)|(,[\t\n\f\r ]*))/;
 
 const kGrammar: {[key: string]: RegExp[]}  = {
