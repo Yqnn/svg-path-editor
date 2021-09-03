a4b1c681cf3f69cef1b139cdf61dceb9d2663b71
Yann Armelin <yann@armelin.fr>
2021-09-03 19:37:23 +0200

    Added auto-saving of configuration

---
+++ src/app/config.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+
+
+function save() {
+  return function(target: Object, propertyKey: string) { 
+    const localStorageKey = `SaveDecorator.${target.constructor.name}.${propertyKey}`;
+    const storedValue = localStorage.getItem(localStorageKey);
+    let value: any = JSON.parse(storedValue ?? 'null');
+    const wasStored = storedValue !== null;
+    let isInitialized = false;
+
+    const setter = (newVal: any) => {
+      if(!wasStored || isInitialized) {
+        value = newVal;
+        if(isInitialized) {
+          localStorage.setItem(localStorageKey, JSON.stringify(value));
+        }
+      }
+      isInitialized = true;
+    };
+
+    Object.defineProperty(target, propertyKey, {
+      get: () => value,
+      set: setter
+    }); 
+  }
+}
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+  @save() viewPortX = 0;
+  @save() viewPortY = 0;
+  @save() viewPortWidth = 30;
+  @save() viewPortHeight = 30;
+  @save() viewPortLocked = false;
+  @save() filled = true;
+  @save() preview = false;
+  @save() showTicks = false;
+  @save() minifyOutput = false;
+  @save() snapToGrid = true;
+  @save() tickInterval = 5;
+  @save() decimalPrecision = 3;
+}
