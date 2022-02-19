756d7969a894b43ed4f3775a97f25c1891c451b6
Yann Armelin <yann@armelin.fr>
2022-02-19 16:46:05 +0100

    Refreshed gh-pages from master

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0";
+  var SW_VERSION = "13.2.3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
