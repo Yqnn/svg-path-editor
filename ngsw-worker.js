c715fc0add00abd7bb2ff74721959d215454bc9e
Yann Armelin <yann@armelin.fr>
2024-09-01 12:59:05 +0200

    Refreshed gh-pages from master

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/darwin_arm64-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.2";
+  var SW_VERSION = "17.3.12";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
